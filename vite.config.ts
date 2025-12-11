bab2eac08ee673b73fe901d45c95170f13a10124
Travis Uhrig <travisformayor@users.noreply.github.com>
2025-12-11 13:34:08 -0700

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-});
+  base: '/geobacter-sim/', // for github pages deploy
+})
